bef80c1ea832e4653d869a645797cdce8174fcb8
smile <rhtsjz@gmail.com>
2018-04-15 16:00:50 +0800

    change project name

--- status.sh
+++ status.sh
@@ -4,7 +4,7 @@ statusCode=$(curl -s -o /dev/null -w "%{http_code}" http://xq.5tree.cn/)
 #echo $statusCode
 if [ $statusCode -eq 200 ]
 then
-   sh /Users/smile/Desktop/develop/wechat_bot/status.sh/ding.sh
+   sh /Users/smile/Desktop/develop/ding_robot/status.sh/ding.sh
 else
    echo $now $statusCode
 fi
