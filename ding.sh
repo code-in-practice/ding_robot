1fb55024d12abc9df070f403ecc641ef11e3fbe3
zhangshijun <zhangshijun@vipkid.com.cn>
2018-04-17 10:32:07 +0800

    合并到一个文件内

--- ding.sh
+++ ding.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-res=$(curl -s -X POST \
-  'https://oapi.dingtalk.com/robot/send?access_token=65aa08d5709cb58e4ecbade5c910e46b09ebee91d852359351b2d9314b7d728d' \
-  -H 'content-type: application/json' \
-  -d '{
-  "msgtype": "link",
-  "link": {
-    "text": "点击打开网站",
-    "title": "西青幼儿园报名网站可以访问",
-    "picUrl": "",
-    "messageUrl": "http://xq.5tree.cn/"
-  }
-}')
-echo $res
+send_ding()
+{
+    res=$(curl -s -X POST \
+      'https://oapi.dingtalk.com/robot/send?access_token=65aa08d5709cb58e4ecbade5c910e46b09ebee91d852359351b2d9314b7d728d' \
+      -H 'content-type: application/json' \
+      -d '{
+      "msgtype": "link",
+      "link": {
+        "text": "点击打开网站",
+        "title": "西青幼儿园报名网站可以访问",
+        "picUrl": "",
+        "messageUrl": "http://xq.5tree.cn/"
+      }
+    }')
+    echo $res
+}
+
+now=$(date)
+statusCode=$(curl -s -o /dev/null -w "%{http_code}" http://xq.5tree.cn/)
+#echo $statusCode
+if [ $statusCode -eq 200 ]
+then
+        send_ding
+    else
+        echo $now $statusCode
+fi
